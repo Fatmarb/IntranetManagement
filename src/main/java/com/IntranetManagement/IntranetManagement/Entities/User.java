1b29f9e120699f065507f70819d80647ae4c9368
KhalilHAJRI <khalil.hajri@esen.tn>
2024-12-05 23:18:53 +0100

    success token generation

--- src/main/java/com/IntranetManagement/IntranetManagement/Entities/User.java
+++ src/main/java/com/IntranetManagement/IntranetManagement/Entities/User.java
@@ -1,83 +1,119 @@
 package com.IntranetManagement.IntranetManagement.Entities;
 
 import jakarta.persistence.*;
-import java.util.Set;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-    @Entity
-    public class User {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        public Long id;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
-        @Column(nullable = false, unique = true)
-        public String username;
+@Table(name = "users")
+@Entity
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Integer id;
 
-        @Column(nullable = false)
-        public String password;
-        @Column(nullable = false)
-        public String FirstName;
-        @Column(nullable = false)
-        public String LastName;
-        @Column(nullable = false)
-        public int IsAdmin;
-        @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-        private Set<Rights> rights;
+    @Column(nullable = false)
+    private String fullName;
 
-        // Getters and Setters
+    @Column(unique = true, length = 100, nullable = false)
+    private String email;
 
-        public Long getId() {
-            return id;
-        }
+    @Column(nullable = false)
+    private String password;
 
-        public void setId(Long id) {
-            this.id = id;
-        }
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
 
-        public String getUsername() {
-            return username;
-        }
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
 
-        public void setUsername(String username) {
-            this.username = username;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-        public String getFirstName() {
-            return FirstName;
-        }
-
-        public void setFirstName(String firstName) {
-            FirstName = firstName;
-        }
-
-        public String getLastName() {
-            return LastName;
-        }
-
-        public void setLastName(String lastName) {
-            LastName = lastName;
-        }
-
-        public int getIsAdmin() {
-            return IsAdmin;
-        }
-
-        public void setIsAdmin(int isAdmin) {
-            IsAdmin = isAdmin;
-        }
-        public Set<Rights> getRights() {
-            return rights;
-        }
-
-        public void setRights(Set<Rights> rights) {
-            this.rights = rights;
-        }
+    public Integer getId() {
+        return id;
     }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public User setFullName(String fullName) {
+        this.fullName = fullName;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
--- src/main/java/com/IntranetManagement/IntranetManagement/repositories/UserRepository.java
+++ src/main/java/com/IntranetManagement/IntranetManagement/repositories/UserRepository.java
@@ -8,7 +8,5 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-    @Query("SELECT u FROM User u JOIN FETCH u.rights r JOIN FETCH r.department WHERE u.username = :username")
-    Optional<User> findUserWithRoles(String username);
+    Optional<User> findByEmail(String email);
 }
